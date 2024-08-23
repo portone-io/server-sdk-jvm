20f89923f22a24556521831e03365100b91b57da
finalchild <me@finalchild.dev>
2024-08-23 17:53:57 +0900

    chore: change rootProject.name to portone-server-sdk-jvm

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,5 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-rootProject.name = "portone-jvm"
+rootProject.name = "portone-server-sdk-jvm"
 include("common")
